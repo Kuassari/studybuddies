55751deb8820c5369b93e3a04ef5d361b6e5aa57
Ofir Sasson <ofir.sasson@biodata.com>
2022-02-28 17:11:13 +0200

    added files and organized everything

---
+++ studybuddies/src/main/java/com/project/studybuddies/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.project.studybuddies.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class LoginController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showLoginPage(ModelMap model) {
+        model.put("name", "StudyBuddy");
+        return "Welcome!!";
+    }
+}
