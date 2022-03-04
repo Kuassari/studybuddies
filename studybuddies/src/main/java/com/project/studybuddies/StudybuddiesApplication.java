afcd69d7d08ea23720f811512fc00d0a132376e4
Ofir Sasson <ofir.sasson@biodata.com>
2022-03-04 22:59:04 +0200

    Update StudybuddiesApplication.java

--- studybuddies/src/main/java/com/project/studybuddies/StudybuddiesApplication.java
+++ studybuddies/src/main/java/com/project/studybuddies/StudybuddiesApplication.java
@@ -1,13 +1,46 @@
 package com.project.studybuddies;
 
+import com.sun.faces.config.ConfigureListener;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import javax.faces.webapp.FacesServlet;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
 public class StudybuddiesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudybuddiesApplication.class, args);
+	}
+
+	/*
+	 * Below sets up the Faces Servlet for Spring Boot
+	 */
+	@Bean
+	public FacesServlet facesServlet() {
+		return new FacesServlet();
+	}
+
+	@Bean
+	public ServletRegistrationBean facesServletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(
+				facesServlet(), "*.xhtml");
+		registration.setName("FacesServlet");
+		return registration;
+	}
+
+	@Bean
+	public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+		return new ServletListenerRegistrationBean<ConfigureListener>(
+				new ConfigureListener());
 	}
 
 }
