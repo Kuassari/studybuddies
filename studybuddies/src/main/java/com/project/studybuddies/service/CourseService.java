ad4ed9b44ceb30e6699135ebfe20e99bfa662d56
Ofir Sasson <ofir.sasson@biodata.com>
2022-03-04 22:59:51 +0200

    Update CourseService.java

--- studybuddies/src/main/java/com/project/studybuddies/service/CourseService.java
+++ studybuddies/src/main/java/com/project/studybuddies/service/CourseService.java
@@ -6,7 +6,13 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import com.project.studybuddies.model.CourseModel;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CourseService {
 
+    private int counter = 0;
+    public String getMessage() {
+        return "This is my message " + counter++;
+    }
 }
